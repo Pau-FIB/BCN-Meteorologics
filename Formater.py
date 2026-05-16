977f30812a7b55f5a08d74b3f1c09fb7eeda452f
Taya <prymataya@gmail.com>
2026-05-16 17:58:38 +0200

    formater corregido

--- Formater.py
+++ Formater.py
@@ -1,29 +1,11 @@
 # #wget -O "duckdb.jar" "https://repo1.maven.org/maven2/org/duckdb/duckdb_jdbc/0.10.1/duckdb_jdbc-0.10.1.jar"
+import os
+
 import pyspark
 from pyspark.sql import SparkSession
 import duckdb
 import unicodedata
 
-# ── Funciones de limpieza ──────────────────────────────────────────────────
-
-def clean_table_name(name):
-    # Limpia el nombre del archivo para usarlo como tabla
-    name = name.split("/")[-1] # Nos quedamos solo con el nombre del archivo, no la ruta
-    name = name.replace(".csv", "").lower().replace(" ", "").replace("-", "_")
-    if name[0].isdigit():
-        name = "t" + name
-    return name
-
-def clean_column(col):
-    # Limpia los nombres de las columnas (acentos, espacios, etc)
-    col = col.replace('\ufeff', '')
-    col = col.replace('"', '')
-    col = col.strip().lower().replace(" ", "_")
-    col = ''.join(
-        c for c in unicodedata.normalize('NFD', col)
-        if unicodedata.category(c) != 'Mn'
-    )
-    return col
 
 # ── Configuración inicial ──────────────────────────────────────────────────
 
@@ -50,18 +32,18 @@ for ruta in archivos:
     df = spark.read.option("delimiter", ",").option("header", True).option("inferSchema", True).csv(ruta)
     
     # 2. Limpiar columnas: Aplicamos clean_column a cada nombre
-    new_columns = [clean_column(c) for c in df.columns]
-    df = df.toDF(*new_columns)
+    #new_columns = [clean_column(c) for c in df.columns]
+    #df = df.toDF(*new_columns)
     
     # 3. Determinar nombre de tabla limpio
-    table_name = clean_table_name(ruta)
-    
+    #table_name = clean_table_name(ruta)
+    table_name = os.path.basename(ruta).replace(".csv", "")
     # 4. Escribir en DuckDB
     print(f"Guardando tabla: {table_name}...")
     df.write \
         .format("jdbc") \
         .option("url", JDBC_URL) \
-        .option("dbtable", table_name) \
+        .option("dbtable", f'"{table_name}"') \
         .option("driver", DRIVER) \
         .mode("overwrite") \
         .save()

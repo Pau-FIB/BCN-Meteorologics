8b3098cfff3868a1963936c673702e38c2b0e942
Taya <prymataya@gmail.com>
2026-05-16 18:05:37 +0200

    codigo limpio

--- Formatter_zone/Formater.py
+++ Formatter_zone/Formater.py
@@ -28,17 +28,11 @@ archivos = [
 ]
 
 for ruta in archivos:
-    # 1. Leer
+    # Leer
     df = spark.read.option("delimiter", ",").option("header", True).option("inferSchema", True).csv(ruta)
-    
-    # 2. Limpiar columnas: Aplicamos clean_column a cada nombre
-    #new_columns = [clean_column(c) for c in df.columns]
-    #df = df.toDF(*new_columns)
-    
-    # 3. Determinar nombre de tabla limpio
-    #table_name = clean_table_name(ruta)
     table_name = os.path.basename(ruta).replace(".csv", "")
-    # 4. Escribir en DuckDB
+    
+    # Escribir en DuckDB
     print(f"Guardando tabla: {table_name}...")
     df.write \
         .format("jdbc") \
